288c307a22453525a1d44b8d8664a8c9c5accb1a
Ranjith <ranjithmenon112@gmail.com>
2022-11-22 12:12:49 +0530

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var firstNum = "";
+var secondNum = "";
 var result = "";
 var operator = "";
 
@@ -6,29 +7,38 @@ function makeNum(num) {
     if(document.getElementById("ans").innerHTML.toString != 0){
         firstNum = firstNum + num;
     }
-    document.getElementById("ans").innerHTML = result + operator + firstNum;
+    if(operator != ""){
+        document.getElementById("opeText").innerHTML = secondNum + operator;
+    }
+    document.getElementById("ans").innerHTML = firstNum;
 }
 
 function allClear(){
     firstNum = "";
+    secondNum ="";
     result = "";
     operator = "";
     document.getElementById("ans").innerHTML = "0";
+    document.getElementById("opeText").innerHTML = "0";
+
 }
 
 function ope(operatorPassed){
     operator = operatorPassed;
-    result = firstNum;
+    secondNum = firstNum;
     firstNum = "";
-    document.getElementById("ans").innerHTML = result + operator;
+    document.getElementById("ans").innerHTML = operator;
+    document.getElementById("opeText").innerHTML = secondNum;
 }
 
 function calculate(){
     switch (operator){
-        case '+' : result = parseInt(result) + parseInt(firstNum) ; break;
-        case '-' : result = parseInt(result) - parseInt(firstNum) ; break;
-        case '*' : result = parseInt(result) * parseInt(firstNum) ; break;
-        case '/' : result = parseInt(result) / parseInt(firstNum) ; break;
+        case '+' : result = parseInt(secondNum) + parseInt(firstNum) ; break;
+        case '-' : result = parseInt(secondNum) - parseInt(firstNum) ; break;
+        case '*' : result = parseInt(secondNum) * parseInt(firstNum) ; break;
+        case '/' : result = parseInt(secondNum) / parseInt(firstNum) ; break;
     }
+    document.getElementById("opeText").innerHTML = secondNum + operator + firstNum;
     document.getElementById("ans").innerHTML = result;
+    secondNum = result;
 }
